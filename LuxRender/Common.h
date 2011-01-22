f6139324cfc2808ad1c9fa4aa4e4d6b12c105660
Asbj?rn Heid <lordcrc@gmail.net>
2011-01-22 17:34:51 +0100

    Removed old LuxMax files.

--- LuxRender/Common.h
+++
@@ -1,9 +0,0 @@
-#include "LuxMax.h"
-
-float Mtl_GetFloat(MtlBase* base, TSTR pr, TimeValue t);
-int Mtl_GetInt(MtlBase* base, TSTR pr, TimeValue t);
-TSTR Mtl_GetStr(MtlBase* base, TSTR pr, TimeValue t);
-Point3 Mtl_GetColor(MtlBase* base, TSTR pr, TimeValue t);
-Texmap* Mtl_GetTexmap(MtlBase* base, TSTR pr, TimeValue t);
-void CommaScan(TCHAR* buf);
-TSTR Format(float value);
--- LuxRender/LuxSurfaceIntegrator.cpp
+++
@@ -1,1 +0,0 @@
-#include "LuxMax.h"
