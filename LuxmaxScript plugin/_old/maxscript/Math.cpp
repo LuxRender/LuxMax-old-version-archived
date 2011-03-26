878eda4b8f7ce76c5c960575f542af0d87415f87
gplourde <gplourde@localhost>
2011-03-25 23:12:51 -0400

    cleanup

---
+++ LuxmaxScript plugin/_old/maxscript/Math.cpp
@@ -0,0 +1,51 @@
+#include "LuxMax.h"
+
+#define DEGTORAD(Deg) ((Deg * 3.141592653589793238) / 180.0)
+#define RADTODEG(Rad) ((180.0 * Rad) / 3.141592653589793238)
+
+// Scale point3 to world scale. LuxRender 1 unit = 1 meters.
+
+Point3 ScaleVector(Point3 p, float scale)
+{
+	p.x /= scale;
+	p.y /= scale;
+	p.z /= scale;
+
+	return p;
+}
+
+Point3 GetVertexNormal(Mesh* mesh, int faceNo, RVertex* rv)
+{
+	Face* f = &mesh->faces[faceNo];
+	DWORD smGroup = f->smGroup;
+	int numNormals;
+	Point3 vertexNormal;
+
+	if (rv->rFlags & SPECIFIED_NORMAL)
+	{
+		vertexNormal = rv->rn.getNormal();
+	}
+	else if ((numNormals = rv->rFlags & NORCT_MASK) && smGroup)
+	{
+		if (numNormals == 1)
+		{
+			vertexNormal = rv->rn.getNormal();
+		}
+		else
+		{
+			for (int i = 0; i < numNormals; i++)
+			{
+				if (rv->ern[i].getSmGroup() & smGroup)
+				{
+					vertexNormal = rv->ern[i].getNormal();
+				}
+			}
+		}
+	}
+	else
+	{
+		vertexNormal = mesh->getFaceNormal(faceNo);
+	}
+
+	return vertexNormal;
+}
